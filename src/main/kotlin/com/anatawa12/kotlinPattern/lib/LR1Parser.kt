4a4f985c9e3f6d5df7f1621565cc0d4d4b6d4549
anatawa12 <anatawa12@icloud.com>
2020-01-01 19:16:52 +0900

    add add LR1Parser and Test

---
+++ src/main/kotlin/com/anatawa12/kotlinPattern/lib/LR1Parser.kt
@@ -0,0 +1,10 @@
+package com.anatawa12.kotlinPattern.lib
+
+/**
+ * Created by anatawa12 on 2020/01/01.
+ */
+class LR1Parser<TToken : IToken>(parserTable: ParserTable, tokens: Sequence<TToken>) {
+    fun runActions(): Any? {
+        TODO()
+    }
+}
